f9fc63ea7ef36de923fb0661d4407991cbdb6b63
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-08-18 11:16:46 +0530

    update

---
+++ CPP Codes/8_Reverse_String.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<char> str = {'h', 'e', 'l', 'l', 'o'};
+    solution.reverseString(str);
+    
+    for (char c : str) {
+        cout << c;
+    }
+    cout << endl;
+    
+    return 0;
+}
