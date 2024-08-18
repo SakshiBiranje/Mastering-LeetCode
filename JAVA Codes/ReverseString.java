416eb1596a2e7d552d21362a4c63bf3c92948b03
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-08-18 11:16:56 +0530

    update

---
+++ JAVA Codes/ReverseString.java
@@ -0,0 +1,26 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+        ReverseString solution = new ReverseString();
+        char[] str = {'h', 'e', 'l', 'l', 'o'};
+        solution.reverseString(str);
+        
+        for (char c : str) {
+            System.out.print(c);
+        }
+        System.out.println();
+    }
+}
