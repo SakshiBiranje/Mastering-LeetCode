7b29ec692c3ef1f1801ec3bda927de6bc9e8a2ef
SakshiBiranje <sakshibiranje7126@gmail.com>
2024-09-03 17:18:33 +0530

    update

---
+++ JAVA Codes/ArrayToQueue.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ArrayToQueue {
+    public static void main(String[] args) {
+        // Example array
+        String[] array = {"A", "B", "C", "D"};
+
+        // Convert array to Queue
+        Queue<String> queue = new LinkedList<>();
+
+        for (String element : array) {
+            queue.add(element);
+        }
+
+        // Display the queue
+        System.out.println("Queue: " + queue);
+    }
+}
