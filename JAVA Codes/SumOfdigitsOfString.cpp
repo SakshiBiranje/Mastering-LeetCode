96d8bdcffa86a7f287208672c8eed11d2a77c746
SAKSHI SANTOSH BIRANJE <161456501+SakshiBiranje@users.noreply.github.com>
2024-09-03 16:47:21 +0530

    Update SumOfdigitsOfString.cpp

--- JAVA Codes/SumOfdigitsOfString.cpp
+++ JAVA Codes/SumOfdigitsOfString.cpp
@@ -1,32 +1,28 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-int sumOfDigits(string num) {
-    int sum = 0;
-    for (char c : num) {
-        sum += c - '0';
-    }
-    return sum;
-}
-
-int getLucky(string s, int k) {
-    string num;
-    for (char c : s) {
-        num += to_string(c - 'a' + 1);
+public class SumOfdigitsOfString {
+    public static int sumOfDigits(String num) {
+        int sum = 0;
+        for (char c : num.toCharArray()) {
+            sum += c - '0';
+        }
+        return sum;
     }
 
-    while (k--) {
-        num = to_string(sumOfDigits(num));
+    public static int getLucky(String s, int k) {
+        StringBuilder num = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            num.append(c - 'a' + 1);
+        }
+
+        while (k-- > 0) {
+            num = new StringBuilder(Integer.toString(sumOfDigits(num.toString())));
+        }
+
+        return Integer.parseInt(num.toString());
     }
 
-    return stoi(num);
-}
-
-int main() {
-    string s = "zbax";
-    int k = 2;
-    cout << getLucky(s, k) << endl;  // Output: 8
-    return 0;
+    public static void main(String[] args) {
+        String s = "zbax";
+        int k = 2;
+        System.out.println(getLucky(s, k));  // Output: 8
+    }
 }
